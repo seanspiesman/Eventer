008f4c5b5508eddf1514303f1e5666dc601f9917
seanspiesman <57199893+seanspiesman@users.noreply.github.com>
2020-05-06 17:27:47 -0500

    Initial Commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Events</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
