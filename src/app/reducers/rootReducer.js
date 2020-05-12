62b0777f3e7c997c01cd4fc3412477337031ca13
seanspiesman <57199893+seanspiesman@users.noreply.github.com>
2020-05-11 20:27:45 -0500

    setup basic crud operators w/ redux

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import testReducer from "../../testarea/testReducer";
-
+import eventReducer from "../EventComponents/eventReducer";
 
 const rootReducer = combineReducers({
   test: testReducer,
+  events: eventReducer,
 });
 
-export default rootReducer
+export default rootReducer;
